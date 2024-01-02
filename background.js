6351df62e4e2d79338cf4c019c2a0cd1a04121d3
phucvinh57 <npvinh0507>
2024-01-02 21:28:09 +0700

    feat: catch tab change and window navigation events

--- background.js
+++ background.js
@@ -6,26 +6,39 @@
  * }} ChromeTab
  */
 
+const APP_NAME = "com.ticklab.tpulse";
 /**
  * 
  * @param {ChromeTab} tab 
  */
 function tabHandler(tab) {
     const unixTs = Math.floor(Date.now() / 1000);
-    const data = {
+    const tabInformation = {
         time: unixTs,
         windowId: tab.windowId,
         title: tab.title,
         url: tab.url
     }
 
-    // TODO: send data to tpulse
-    console.log(data);
+    // console.log(tabInformation);
+    chrome.runtime.sendNativeMessage(APP_NAME, tabInformation)
+        .catch((err) => console.error(err));
 }
 
-chrome.tabs.onActivated.addListener(function (activeInfo) {
-    const tabId = activeInfo.tabId;
-    chrome.tabs.get(tabId, tabHandler);
-});
+function watch() {
+    chrome.tabs.onActivated.addListener(function (activeInfo) {
+        const tabId = activeInfo.tabId;
+        console.log("tabs.onActivated");
+        chrome.tabs.get(tabId, tabHandler);
+    });
+    chrome.webNavigation.onCommitted.addListener(function (details) {
+        // Check if the navigation is to the main frame
+        if (details.frameId === 0) {
+            console.log("webNavigation.onCommitted");
+            const tabId = details.tabId;
+            chrome.tabs.get(tabId, tabHandler);
+        }
+    });
+}
 
-
+watch();
